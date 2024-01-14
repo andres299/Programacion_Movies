ccd960743805ee00e537776ddc3d9907bb1ff9d5
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-01-14 19:26:14 +0100

    añadir Movie_Genres

---
+++ src/main/java/com/esliceu/demoMovies/Entities/Movie_Genres.java
@@ -0,0 +1,47 @@
+package com.esliceu.demoMovies.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(
+        name = "movie_genres",
+        uniqueConstraints =
+        @UniqueConstraint(columnNames = {"movie_id", "genre_id"})
+)
+public class Movie_Genres {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "genre_id")
+    private Genre genre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+}
