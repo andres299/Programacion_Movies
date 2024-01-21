f66e2f44b7f5b1be515b94c37a36c7469d671a29
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-01-21 19:17:43 +0100

    correccion CompanyRepo

--- src/main/java/com/esliceu/demoMovies/Repositorys/Movie_CompanyRepo.java
+++ src/main/java/com/esliceu/demoMovies/Repositorys/Movie_CompanyRepo.java
@@ -1,12 +1,13 @@
 package com.esliceu.demoMovies.Repositorys;
 
+import com.esliceu.demoMovies.Entities.Movie_Company;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface Movie_CompanyRepo extends JpaRepository<Movie_CompanyRepo, Long> {
+public interface Movie_CompanyRepo extends JpaRepository<Movie_Company, Long> {
     @Transactional
     @Modifying
     @Query(value = "DELETE FROM movie_company WHERE company_id = :entityId", nativeQuery = true)
