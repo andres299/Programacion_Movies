3586cd80cbd00555e0f98bb9d3c4b140298faa42
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-01-21 18:46:10 +0100

    correccion

--- src/main/java/com/esliceu/demoMovies/Repositorys/Movie_LanguagesRepo.java
+++ src/main/java/com/esliceu/demoMovies/Repositorys/Movie_LanguagesRepo.java
@@ -1,13 +1,14 @@
 package com.esliceu.demoMovies.Repositorys;
 
 import com.esliceu.demoMovies.Entities.Administrator;
+import com.esliceu.demoMovies.Entities.Movie_Languages;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface Movie_LanguagesRepo extends JpaRepository<Movie_LanguagesRepo, Long> {
+public interface Movie_LanguagesRepo extends JpaRepository<Movie_Languages, Long> {
     @Transactional
     @Modifying
     @Query(value = "DELETE FROM movie_languages WHERE language_id = :entityId", nativeQuery = true)
