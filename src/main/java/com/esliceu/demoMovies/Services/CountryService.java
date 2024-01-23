2f048403a8209fd497976a30f62cb2a443707fd5
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-01-23 10:57:40 +0100

    cambios

--- src/main/java/com/esliceu/demoMovies/Services/CountryService.java
+++ src/main/java/com/esliceu/demoMovies/Services/CountryService.java
@@ -1,7 +1,11 @@
 package com.esliceu.demoMovies.Services;
 
+import com.esliceu.demoMovies.Repositorys.CountryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CountryService {
+    @Autowired
+    CountryRepo countryRepo;
 }
--- src/main/java/com/esliceu/demoMovies/Services/GenreService.java
+++ src/main/java/com/esliceu/demoMovies/Services/GenreService.java
@@ -1,7 +1,11 @@
 package com.esliceu.demoMovies.Services;
 
+import com.esliceu.demoMovies.Repositorys.GenreRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenreService {
+    @Autowired
+    GenreRepo genreRepo;
 }
