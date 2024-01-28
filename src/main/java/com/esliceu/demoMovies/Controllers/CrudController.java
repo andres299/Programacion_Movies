c7aa78be7258ecc4f462e64f7660b9d8adff826e
Andres Carrasco Dols <acarrascod@esliceu.net>
2024-01-28 13:35:58 +0100

    cambios

--- src/main/java/com/esliceu/demoMovies/Controllers/CrudController.java
+++ src/main/java/com/esliceu/demoMovies/Controllers/CrudController.java
@@ -30,11 +30,13 @@ public class CrudController {
 
     @GetMapping("/crud")
     public String showCrud(Model model, @RequestParam(defaultValue = "0") int page) {
+        // Obtiene el administrador de la sesión actual.
         Administrator admin = (Administrator) session.getAttribute("admin");
+        // Verifica si el administrador no está autenticado y lo redirige a la página de filtrado de películas.
         if (admin == null) {
             return "redirect:/filterMovies";
         }
-        List<Country> country = countryService.getAllCountrys(page);
+        // Agrega la lista de países al modelo para su uso en la vista.
         model.addAttribute("listCountries", countryService.getAllCountrys(page));
         return "crud";
     }
@@ -42,56 +44,64 @@ public class CrudController {
     @PostMapping("/infoEntities")
     @ResponseBody
     public List<?> crud(@RequestParam String selectedValue,@RequestParam int page){
+        // Retorna una lista de entidades según el parametro seleccionado por el usuario.
         return movieService.infoEntities(selectedValue,page);}
 
     @PostMapping("/searchEntities")
     @ResponseBody
     public List<?> searchEntities(@RequestBody FetchEntitiDTO fetchEntitiDTO) {
-            return movieService.searchEntities(fetchEntitiDTO);
+        // Retorna una lista de entidades, es como un buscador.
+        return movieService.searchEntities(fetchEntitiDTO);
     }
-
     @PostMapping("/operationEntities")
     public ResponseEntity<String> operationEntities(@RequestBody FetchEntitiDTO fetchEntitiDTO) {
+        // Obtiene el administrador de la sesión actual.
         Administrator admin = (Administrator) session.getAttribute("admin");
+        //Obtiene el primer input.
         String input1 = fetchEntitiDTO.getInput1();
         try {
             //Si el primer imput esta vacio devuelve mensaje de error
             if (movieService.inputEntitie(input1)) {
-                //Si esta logeado hace la operacion
+                //Si esta logeado hace la operacion y devuelve un mensaje positivo.
                 if (admin != null) {
                     movieService.operationEntitie(fetchEntitiDTO);
                     return ResponseEntity.ok("Se ha realizado correctamente");
                 } else{
-                // Aquí agrego manejo específico para la excepción relacionada con el administrador
+                    // Devuelve un mensaje de error si el administrador no está autenticado.
                     return ResponseEntity.badRequest().body("Error: El administrador no está autenticado");
                 }
             } else {
+                // Devuelve un mensaje de error si el input no es válido para la entidad.
                 return ResponseEntity.badRequest().body("Error: Entrada no válida para la entidad");
             }
         } catch (UnsupportedOperationException | MovieService.EntityNotFoundException | MovieService.entitiExist e) {
-            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage()); // Manejo de excepciones especificas
         }
     }
 
     @PostMapping("/operationMovies")
     ResponseEntity<String> operationMovies(@RequestBody OperationMovies operationMovies){
+        // Obtiene el administrador de la sesión actual.
         Administrator admin = (Administrator) session.getAttribute("admin");
+        // Obtiene el título de la película del objeto OperationMovies.
         String title = operationMovies.getTitle();
         try {
             //Si el primer imput esta vacio devuelve mensaje de error
             if (movieService.inputEntitie(title)) {
-                //Si esta logeado hace la operacion
+                // Si el administrador está autenticado, realiza la operación en la película.
                 if (admin != null) {
                     movieService.operationMovies(operationMovies);
                     return ResponseEntity.ok("Se ha realizado correctamente");
                 } else{
-                // Aquí agrego un manejo específico para la excepción relacionada con el administrador
+                    // Devuelve un mensaje de error si el administrador no está autenticado.
                      return ResponseEntity.badRequest().body("Error: El administrador no está autenticado");
                 }
             } else {
+                // Devuelve un mensaje de error si el título no es válido para la película.
                 return ResponseEntity.badRequest().body("Error: Entrada no válida para la entidad");
             }
         } catch (UnsupportedOperationException | MovieService.EntityNotFoundException | MovieService.entitiExist e) {
+            // Manejo de excepciones y retorno de mensajes de error específicos.
             return ResponseEntity.badRequest().body("Error: " + e.getMessage());
         }
     }
